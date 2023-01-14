c642e2fbcc304ba970d9aa55a4fda584b28c5d16
ale24dev <ale.24dev@gmail.com>
2023-01-13 22:27:45 -0500

    Improve architecture of News Details view

--- lib/src/feature/news/view/news_details.dart
+++ lib/src/feature/news/view/news_details.dart
@@ -35,109 +35,129 @@ class NewsDetails extends StatelessWidget {
             height: 92.h,
             child: ListView(
               children: [
-                SizedBox(
-                  height: HEIGHT_IMAGE_HEADER,
-                  width: 100.w,
-                  child: Stack(
-                    children: [
-                      SizedBox(
-                        height: HEIGHT_IMAGE_HEADER,
-                        child: Image.network(
-                          newsBaseModel.attachments[0],
-                          fit: BoxFit.cover,
-                          width: 100.w,
-                        ),
-                      ),
-                      Container(
-                        height: HEIGHT_IMAGE_HEADER,
-                        decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                                begin: Alignment.center,
-                                end: Alignment.bottomCenter,
-                                colors: [
-                              GStyles.backGroundDarkColor.withOpacity(.3),
-                              GStyles.backGroundDarkColor
-                            ])),
-                      ),
-                      Positioned(
-                          top: 4.h,
-                          left: 4.w,
-                          child: IconButton(
-                              icon: FaIcon(
-                                FontAwesomeIcons.x,
-                                size: 20.sp,
-                              ),
-                              onPressed: () {
-                                context.router.pop();
-                              }))
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: Constants.MARGIN),
-                  child: Text(
-                    newsBaseModel.title,
-                    softWrap: true,
-                    style: context.textTheme.headline1?.copyWith(
-                        fontFamily: GStyles.fontSanFrancisco, fontSize: 20.sp),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: Constants.MARGIN),
-                  child: SizedBox(
-                    height: HEIGHT_USER_INFO,
-                    child: Row(
-                      children: [
-                        Container(
-                          height: 30.sp,
-                          width: 30.sp,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50)),
-                          child: ClipRRect(
-                              borderRadius: BorderRadius.circular(50),
-                              child: Image.network(newsBaseModel.user.image)),
-                        ),
-                        SizedBox(width: 10.sp),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(newsBaseModel.user.nickname,
-                                style: context.textTheme.bodyText1
-                                    ?.copyWith(fontSize: 16.sp)),
-                            SizedBox(height: 5.sp),
-                            Text(Utils.getDate(newsBaseModel.createdAt),
-                                style: context.textTheme.bodyText1?.copyWith(
-                                    fontSize: 14.sp, color: Colors.white38)),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
+                imageHeader(context),
+                titleSection(context),
+                userInfoSection(context),
                 const Divider(color: Colors.white12),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: Constants.MARGIN),
-                  child: Text(newsBaseModel.text,
-                      softWrap: true,
-                      style: TextStyle(color: Colors.white60, fontSize: 16.sp)),
-                )
+                textNewsSection()
               ],
             ),
           ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: Constants.MARGIN),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  FaIcon(FontAwesomeIcons.solidPaperPlane, size: 17.sp),
-                  FaIcon(FontAwesomeIcons.bookmark, size: 17.sp),
-                ],
-              ),
-            ),
-          )
+          footerNews()
         ],
+      ),
+    );
+  }
+
+  Padding textNewsSection() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: Constants.MARGIN),
+      child: Text(newsBaseModel.text,
+          softWrap: true,
+          style: TextStyle(color: Colors.white60, fontSize: 16.sp)),
+    );
+  }
+
+  Padding userInfoSection(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: Constants.MARGIN),
+      child: SizedBox(
+        height: HEIGHT_USER_INFO,
+        child: Row(
+          children: [
+            Container(
+              height: 30.sp,
+              width: 30.sp,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(50)),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image.network(newsBaseModel.user.image)),
+            ),
+            SizedBox(width: 10.sp),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(newsBaseModel.user.nickname,
+                    style:
+                        context.textTheme.bodyText1?.copyWith(fontSize: 16.sp)),
+                SizedBox(height: 5.sp),
+                Text(Utils.getDate(newsBaseModel.createdAt),
+                    style: context.textTheme.bodyText1
+                        ?.copyWith(fontSize: 14.sp, color: Colors.white38)),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding titleSection(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: Constants.MARGIN),
+      child: Text(
+        newsBaseModel.title,
+        softWrap: true,
+        style: context.textTheme.headline1
+            ?.copyWith(fontFamily: GStyles.fontSanFrancisco, fontSize: 20.sp),
+      ),
+    );
+  }
+
+  SizedBox imageHeader(BuildContext context) {
+    return SizedBox(
+      height: HEIGHT_IMAGE_HEADER,
+      width: 100.w,
+      child: Stack(
+        children: [
+          SizedBox(
+            height: HEIGHT_IMAGE_HEADER,
+            child: Image.network(
+              newsBaseModel.attachments[0],
+              fit: BoxFit.cover,
+              width: 100.w,
+            ),
+          ),
+          Container(
+            height: HEIGHT_IMAGE_HEADER,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.center,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                  GStyles.backGroundDarkColor.withOpacity(.3),
+                  GStyles.backGroundDarkColor
+                ])),
+          ),
+          Positioned(
+              top: 4.h,
+              left: 4.w,
+              child: IconButton(
+                  icon: FaIcon(
+                    FontAwesomeIcons.x,
+                    size: 20.sp,
+                  ),
+                  onPressed: () {
+                    context.router.pop();
+                  }))
+        ],
+      ),
+    );
+  }
+
+  Expanded footerNews() {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: Constants.MARGIN),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            FaIcon(FontAwesomeIcons.solidPaperPlane, size: 18.sp),
+            FaIcon(FontAwesomeIcons.bookmark, size: 18.sp),
+          ],
+        ),
       ),
     );
   }
